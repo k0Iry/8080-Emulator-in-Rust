e26889550ff8c849282640515117a998a0c216fa
Xin Tu <kljsandjb@me.com>
2021-06-15 14:59:02 +0200

    finish chap 11

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,13 +1,17 @@
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 #[cfg(test)]
 mod tests {
+    use super::internal_adder;
     #[test]
     fn exploration() {
         assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
-    fn another() {
-        assert!(false);
     }
     
     #[test]
@@ -17,5 +21,10 @@ mod tests {
         } else {
             Err(String::from("two plus two does not equal four"))
         }
+    }
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
 }
