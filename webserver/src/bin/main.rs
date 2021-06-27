c16350f4a004365e7283dcab2e2b7e216872bcbb
Xin Tu <kljsandjb@me.com>
2021-06-27 18:36:47 +0200

    webserver sample

---
+++ webserver/src/bin/main.rs
@@ -0,0 +1,100 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::BufReader;
+use std::fs;
+use webserver::ThreadPool;
+use std::path::Path;
+use std::ffi::OsStr;
+use std::io::copy;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:9000").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn read_line<T>(reader: &mut BufReader<T>) -> String
+where T: Read {
+    let mut line = String::new();
+    loop {
+        match reader.read_line(&mut line) {
+            Ok(_) => break,
+            Err(e) => panic!("Other IO error: {}", e)
+        }
+    }
+
+    line
+}
+
+fn content_type(filename: &str) -> String {
+    if let Some(extension) = Path::new(filename).extension().and_then(OsStr::to_str)
+    {
+        if extension == "html" {
+            return String::from("Content-Type: ") + "text/html"
+        } else if extension == "mkv" {
+            return String::from("Content-Type: ") + "video/x-matroska"
+        }
+    }
+    String::from("Content-Type: ") + "application/octet-stream"
+}
+
+fn handle_connection(mut stream: TcpStream) {   // need to be mutable, the low-level offset of socket fd is changed
+    let mut reader = BufReader::new(&stream);
+
+    let request_line = read_line(&mut reader);
+
+    println!("HTTP request line read: {}", request_line);
+
+    let mut iter = request_line.split_whitespace();
+    // MATCH: method(GET,POST...) uri(/index.html...) version(which we don't care)
+    let (method, uri, _) = (iter.next().unwrap(), iter.next().unwrap(), iter.next().unwrap());
+
+    println!("HTTP Request Headers:");
+    loop {
+        let request_header = read_line(&mut reader);
+        if request_header == "\r\n" {
+            break
+        }
+        println!("{}", request_header)
+    }
+
+    drop(reader);
+
+    let (status_line, filename) = if method == "GET" {
+        if uri == "/" {
+            ("HTTP/1.1 200 OK", "index.html")
+        } else {
+            match fs::metadata(Path::new(&uri[1..])) {
+                Ok(_) => ("HTTP/1.1 200 OK", &uri[1..]),
+                Err(_) => ("HTTP/1.1 404 NOT FOUND", "404.html")
+            }
+        }
+    } else {
+        ("HTTP/1.1 501 NOT IMPLEMENTED", "404.html")
+    };
+
+    let mut file = std::fs::File::open(filename).unwrap();
+    let response_headers = format!(
+        "{}\r\nContent-Length: {}\r\n{}\r\n\r\n",
+        status_line,
+        file.metadata().unwrap().len(),
+        content_type(filename)
+    );
+
+    if let Ok(_) = stream.write_all(response_headers.as_bytes()) {
+        copy(&mut file, &mut stream).unwrap_or_else(|e| {
+            eprintln!("{:?}", e.kind());
+            0
+        });
+    }
+
+    stream.flush().unwrap();
+}
---
+++ webserver/src/lib.rs
@@ -0,0 +1,102 @@
+use std::thread;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>
+}
+
+type Job = dyn FnOnce() + 'static + Send;
+type JobRef = Box<Job>;
+
+enum Message {
+    NewJob(JobRef),
+    Terminate,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+        let mut workers = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
+        
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)))
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where F: FnOnce() + 'static + Send {
+        let job = Box::new(f);
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>
+}
+
+impl Worker {
+    /// Create a new Worker and start the eventloop
+    ///
+    /// The id is the identifier of thread assigned for worker.
+    /// 
+    /// The receiver is the atomic mutex-based pointer for receive end of MPSC channel
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the lock is failed to acquire (e.g. another thread panicked with lock held).
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            // NOTE: MutexGuard should be dropped once we get the job, be careful with call chains
+            // temporary used in expression will be dropped when expression ends
+            let job = receiver.lock().unwrap().recv().unwrap();
+            match job {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+                    job()
+                },
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+                    break;
+                }
+            };
+        });
+
+        Worker { id, thread: Some(thread) }
+    }
+}
