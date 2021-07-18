1945a9c33855f4c606af167cea280bd0398c65a5
Xin Tu <kljsandjb@me.com>
2021-07-18 16:57:34 +0200

    binary search for median number

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -185,4 +185,44 @@ pub fn find_duplicate(nums: Vec<i32>) -> i32 {
     result
 }
 
+use libc::INT_MIN;
+use libc::INT_MAX;
+use std::cmp;
+
+pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+    let len1 = nums1.len();
+    let len2 = nums2.len();
+    if len1 > len2 {
+        return find_median_sorted_arrays(nums2, nums1)
+    }
+
+    let mut l = 0;
+    let mut r = len1;
+
+    while l <= r {
+        let mid1 = l + (r - l) / 2;
+        let mid2 = (len1 + len2) / 2 - mid1;
+        
+        let l1 = if mid1 == 0 {INT_MIN} else {nums1[mid1 - 1]};
+        let r1 = if mid1 == 0 {INT_MIN} else {nums1[mid1]};
+
+        let l2 = if mid2 == 0 {INT_MAX} else {nums2[mid2 - 1]};
+        let r2 = if mid2 == 0 {INT_MAX} else {nums2[mid2]};
+
+        if l1 > r2 {
+            r = mid1 - 1
+        } else if l2 > r1 {
+            l = mid1 + 1
+        } else {
+            if (len1 + len2) % 2 == 1 {
+                return cmp::min(r1, r2) as f64
+            } else {
+                return (cmp::max(l1, l2) + cmp::min(r1, r2)) as f64 / 2.0;
+            }
+        }
+    }
+
+    0.0
+}
+
 fn main() {}
